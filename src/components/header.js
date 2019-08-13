76f143e40961bab778c3767837eecd72fd706c4c
JABBA\msi <jb@appstab.co>
2019-08-13 10:59:37 +0800

    Restore retailer prices componenet changes fix: Header layout

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import logo from '../SpendaWhale.svg';
 
 function Header() {
-    let user = "Alex"
+    // let user = "Alex"
     return (
         <>
             <header className="main-head d-flex justify-content-between align-items-center">
@@ -11,24 +11,8 @@ function Header() {
                     <img src={logo} className="app-logo ml-3" alt="logo" />
                 </Link>
                 <h1>Spenda Shop</h1>
-                <div><span role="img" aria-label="user-emoji">👨🏽‍💼</span>Hi {user}</div>
+                {/* <div><span role="img" aria-label="user-emoji">👨🏽‍💼</span>Hi {user}</div> */}
                 <nav className="main-nav">
-                    {/* <ul>
-                        <li><button type="button">Nav 1</button></li>
-                        <li><button type="button">Nav 2</button></li>
-                        <li><button type="button">Nav 3</button></li>
-                    </ul> */}
-                    {/* <HamburgerMenu
-                        isOpen={this.state.open}
-                        menuClicked={this.handleClick.bind(this)}
-                        width={18}
-                        height={15}
-                        strokeWidth={1}
-                        rotate={0}
-                        color='black'
-                        borderRadius={0}
-                        animationDuration={0.5}
-                    /> */}
                 </nav>
 
             </header>
--- src/components/home.js
+++ src/components/home.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 function Home() {
     return (
         <>
-            <Link className="btn btn-primary my-4" to="/new">New List</Link>
+            <Link className="btn btn-primary my-4" to="/new">New Shopping List</Link>
             <h2>Home</h2>
         </>
     )
